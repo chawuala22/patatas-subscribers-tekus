e9298dc8a9ef8a77a6a012f8585741f928c81b7e
chawuala <mrojas@econtainers.co>
2023-06-16 20:06:24 -0500

    Se plantea guards para validar usuario logged

--- src/app/pages/pages.routing.ts
+++ src/app/pages/pages.routing.ts
@@ -2,13 +2,10 @@ import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { SubscriberListComponent } from './subscriber-list/subscriber-list.component';
 
-
-const routes: Routes = [
- {path: 'home', component:SubscriberListComponent}
-];
+const routes: Routes = [{ path: 'home', component: SubscriberListComponent }];
 
 @NgModule({
-    imports: [RouterModule.forChild(routes)],
-    exports: [RouterModule]
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
 })
 export class PagesRoutingModule {}
