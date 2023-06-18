ab4f2eb514dd5d615de88efb5e66a9b8b817bded
chawuala <mrojas@econtainers.co>
2023-06-17 23:39:35 -0500

    home view model

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -5,8 +5,21 @@ import { SubscriberDetailComponent } from './subscriber-detail/subscriber-detail
 import { SharedModule } from '../shared/shared.module';
 import { ComponentsModule } from '../components/components.module';
 
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+
 @NgModule({
   declarations: [SubscriberListComponent, SubscriberDetailComponent],
-  imports: [CommonModule, SharedModule, ComponentsModule]
+  imports: [
+    CommonModule,
+    SharedModule,
+    ComponentsModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatInputModule,
+    MatButtonModule
+  ],
 })
 export class PagesModule {}
